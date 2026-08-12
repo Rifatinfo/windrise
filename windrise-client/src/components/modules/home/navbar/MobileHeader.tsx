8e6ffbd24fd9ffe178f62b0277465342331759b4
Rifatinfo <mdrifathossainsinfo@gmail.com>
2026-08-12 17:20:20 +0600

    add cors

--- windrise-client/src/components/modules/home/navbar/MobileHeader.tsx
+++ windrise-client/src/components/modules/home/navbar/MobileHeader.tsx
@@ -2,13 +2,16 @@
 import { MenuIcon, SearchIcon } from 'lucide-react'
 import { IconButton } from './IconButton'
 import WhiteLogo from '@/components/shared/logo/WhiteLogo'
+import BlackLogo from '@/components/shared/logo/BlackLogo'
 type MobileHeaderProps = {
   onMenuOpen: () => void
+  logoVariant?: 'white' | 'black'
 }
-export function MobileHeader({ onMenuOpen }: MobileHeaderProps) {
+export function MobileHeader({ onMenuOpen, logoVariant = 'white' }: MobileHeaderProps) {
+  const Logo = logoVariant === 'white' ? WhiteLogo : BlackLogo
   return (
     <div className="flex h-16 items-center justify-between px-6 ">
-     <WhiteLogo/>
+     <Logo />
       <div className='-mr-2'>
         <IconButton icon={MenuIcon} label="Open menu" onClick={onMenuOpen} />
       </div>
